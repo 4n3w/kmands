4bf50d1839787aa065e20dca405f6c98c631fddd
Andrew Wood <4n3w@users.noreply.github.com>
2022-02-09 12:42:59 -0500

    Remove .sh

--- klibs.bash
+++ klibs.bash
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 check_dependencies(){
-    if [[ ! -x "$(which fzf 2>/dev/null)" ]]; then
-     echo "Please Install: github.com/junegunn/fzf" >&2
-     return 1
-    fi
-    if [[ ! -x "$(which kubectl 2>/dev/null)" ]]; then
-     echo "Please Install: kubectl" >&2
-     return 1
-    fi
-    if [[ ! -x "$(which gshuf 2>/dev/null)" ]]; then
-     echo "Please Install: kubectl" >&2
-     return 1
-    fi
+    R=$1 && REQUIRED_DEPENDENCIES=${R[@]}
+    MISSING_DEPENDENCIES=()                                                                                                    
+                                                                                                                               
+    for DEPENDENCY in ${REQUIRED_DEPENDENCIES[@]}; do                                                                          
+        command -v $DEPENDENCY >/dev/null 2>&1 || { MISSING_DEPENDENCIES+=($DEPENDENCY); }                                       
+    done                                                                                                                       
+                                     
+    # Instead of exiting when we see a missing command, let's be nice and give the user a list.                                
+    if [ ${#MISSING_DEPENDENCIES[@]} -ne 0 ]; then                                                                             
+        printf 'Oops. Missing some dependencies:\n'
+        printf '> Missing %s, please install it!\n' "${MISSING_DEPENDENCIES[@]}"                                                   
+        printf 'Exiting.\n'                                                                                                      
+        exit 1;                                                                                                                  
+    fi  
 }
 
